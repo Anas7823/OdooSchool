4a486a21764333ac1d8659e559d72bd1549b2eaa
Anas7823 <anaselkhiat78@gmail.com>
2024-06-20 09:45:48 +0200

    renommage des liens et renseignement du dev

--- __manifest__.py
+++ __manifest__.py
@@ -1,14 +1,14 @@
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
 {
-    'name': 'school',
+    'name': 'Club manager',
     'summary': 'Formulaire en code',
-    'description': '',
+    'description': 'Ce module permet de générer une page de formulaire d\'inscription pour votre club de foot. Il permet de gérer les inscriptions des joueurs et les paiements.',
     'category': 'Tools',
-    'author': 'aaa',
+    'author': 'Start zup',
     'maintainer': "",
-    'company': '',
-    'website': '',
+    'company': 'Start zup',
+    'website': 'https://start-zup.com/',
     'license': 'AGPL-3',
     'depends': ['base', 'mail', 'sale_management', 'website', 'payment_stripe'],
     'data': [
